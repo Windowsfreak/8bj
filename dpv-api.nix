e07c65d7f5e8195555237ea503198ba07aead425
Björn Eberhardt <beberhar@thoughtworks.com>
2024-02-19 15:16:16 +0100

    use systemd to create a folder in /var/run

--- dpv-api.nix
+++ dpv-api.nix
@@ -10,11 +10,12 @@
     wantedBy = [ "multi-user.target" ];
     after = [ "network.target" ];
     script = ''
-      export UNIX=/var/run/apiserver1.sock
+      export UNIX=/var/run/dpv1/apiserver.sock
       exec /var/dpv/api/bin/endpoint1
     '';
     serviceConfig = {
       WorkingDirectory = "/var/dpv/api";
+      RuntimeDirectory = "dpv1";
       User = "dpv";
       Group = "dpv";
       Restart = "always";
