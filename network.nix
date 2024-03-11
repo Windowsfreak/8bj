3a0df094ba3fee26e9767e1aa1c8a8a74ec945ea
Björn Eberhardt <beberhar@thoughtworks.com>
2024-03-11 22:06:40 +0100

    introduce wireguard

--- network.nix
+++ network.nix
@@ -13,7 +13,7 @@ in
     hostName = "8bj";
     firewall = {
       allowedTCPPorts = [ 22 80 443 8529 8123 993 995 465 ];
-      allowedUDPPorts = [ 443 ];
+      allowedUDPPorts = [ 443 51820 ];
       logRefusedConnections = false;
     };
     useDHCP = false;
@@ -33,5 +33,27 @@ in
       interface = "ens18";
     };
     nameservers = [ "8.8.8.8" "8.8.4.4" ];
+    nat = {
+      enable = true;
+      externalInterface = "ens18";
+      internalInterfaces = [ "wg0" ];
+    };
+    wireguard.interfaces.wg0 = {
+      ips = [ "10.100.0.1/24" ];
+      listenPort = 51820;
+      postSetup = ''
+        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o ens18 -j MASQUERADE
+      '';
+      postShutdown = ''
+        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o ens18 -j MASQUERADE
+      '';
+      privateKeyFile = "/var/config/wireguard/private.key";
+      peers = [
+        { # i7
+          publicKey = "8GDBnOggqwchSPeLEvOCP16zdDRwP2his5PoyIQ5I3o=";
+          allowedIPs = [ "10.100.0.2/32" "10.100.0.3/32" "10.100.0.4/32" "10.100.0.5/32" "10.100.0.6/32" ];
+        }
+      ];
+    };
   };
 }
