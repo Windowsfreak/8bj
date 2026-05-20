071099cbeafededc1900f5069eab83d323d3e005
Björn Eberhardt <beberhar@thoughtworks.com>
2026-05-20 11:33:33 +0200

    Move gpt settings to environment.etc.rspamd/local.d/gpt.conf.text

--- mailserver.nix
+++ mailserver.nix
@@ -236,22 +236,23 @@ Tel.: 040-6552347 | Fax: 040-65590732";
       sieve_redirect_envelope_from = orig_recipient
     }
   '';
+  environment.etc."rspamd/local.d/gpt.conf".text = ''
+    enabled = true;
+    allow_ham = true;
+    type = "openai";
+    .include(try=true) "/var/config/rspamd-gpt-secret.conf"
+    model = "qwen-3-235b-a22b-instruct-2507";
+    max_tokens = 1000;
+    temperature = 0.7;
+    top_p = 0.9;
+    timeout = 10s;
+    autolearn = true;
+    url = "http://localhost:3001/v1/chat/completions";
+    prompt = "Analyze this email strictly as a spam detector given the email message, subject, FROM and url domains. Your recipient lives in Hamburg, Germany, runs a business, kindergarden, parkour organisation, live music performance and trades cryptocurrencies. Evaluate spam probability (0-1). Output ONLY 3 lines:\n1. Numeric score (0.00-1.00)\n2. One-sentence reason citing strongest red flag\n3. Primary concern category if found from the list: malware, phishing, marketing, scam";
+    reason_header = "X-GPT-Reason";
+  '';
+
   services.rspamd.extraConfig = lib.mkAfter ''
-gpt {
-  enabled = true;
-  allow_ham = true;
-  type = "openai";
-  .include(try=true) "/var/config/rspamd-gpt-secret.conf"
-  model = "qwen-3-235b-a22b-instruct-2507";
-  max_tokens = 1000;
-  temperature = 0.7;
-  top_p = 0.9;
-  timeout = 10s;
-  autolearn = true;
-  url = "http://localhost:3001/v1/chat/completions";
-  prompt = "Analyze this email strictly as a spam detector given the email message, subject, FROM and url domains. Your recipient lives in Hamburg, Germany, runs a business, kindergarden, parkour organisation, live music performance and trades cryptocurrencies. Evaluate spam probability (0-1). Output ONLY 3 lines:\n1. Numeric score (0.00-1.00)\n2. One-sentence reason citing strongest red flag\n3. Primary concern category if found from the list: malware, phishing, marketing, scam";
-  reason_header = "X-GPT-Reason";
-}
 settings {
   local {
     priority = high;
