be55a4fa2f625b7d1f277673ac51992dbccbc8b9
Björn Eberhardt <beberhar@thoughtworks.com>
2026-07-27 01:12:08 +0200

    chore: upgrade postgresql to version 16 and update plugin reference in configuration

--- configuration.nix
+++ configuration.nix
@@ -188,8 +188,9 @@
       settings.PasswordAuthentication = false;
     };
     postgresql = {
-      extraPlugins = with pkgs.postgresql.pkgs; [ 
-        pgvector 
+      package = pkgs.postgresql_16;
+      extraPlugins = [
+        pkgs.postgresql_16.pkgs.pgvector
       ];
       authentication = ''
         # "local" is for Unix domain socket connections only
